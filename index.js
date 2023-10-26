caa8937ef56840f23c3d8a85d881f4b7a17c5c9e
skrg110 <141757219+skrg110@users.noreply.github.com>
2023-10-26 22:37:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="YOUR URL";
+var hostURL="6751021259:AAHs6WEvuS5kE_hfOeF-UnqtQ2LS-nw4fls";
 //TOGGLE for 1pt Proxy and Shorters
 var use1pt=true;
 
